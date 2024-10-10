148935b56681b3ea4f1f534ee1936dd4d76d4a92
JosepharDev <josephardev@gmail.com>
2024-10-10 09:22:40 -0700

    add to-str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" write a type annotated function to_str """
+
+
+def to_str(n: float) -> str:
+    """ takes a float n and return string representation of float """
+    return str(n)
