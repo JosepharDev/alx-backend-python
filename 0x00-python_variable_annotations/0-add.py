54ef716cbf080492c13e2ce93e12291b69a6ccf9
JosepharDev <josephardev@gmail.com>
2024-10-10 08:56:03 -0700

    fucntion documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+""" write a type-annotated function add """
+
 def add(a: float, b: float) -> float:
+    """ takes two argument float and return float """
     return a + b
