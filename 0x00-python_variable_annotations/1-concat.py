81b12df947289113435c46cd8ff0766ebaa255f2
JosepharDev <josephardev@gmail.com>
2024-10-10 09:16:36 -0700

    fix pycodestype

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,6 @@
 """ write a type-annotated function concat """
 
 
-def concat(str1: str, str2: str -> str:
+def concat(str1: str, str2: str) -> str:
     """ takes two argument type string and return concatinated string """
     return str1 + str2
